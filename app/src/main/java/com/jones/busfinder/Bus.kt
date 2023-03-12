f0c4796b43e42ccf9b75cb1e131b396009c215a5
juodejones2309 <112094375+juodejones2309@users.noreply.github.com>
2023-03-12 21:20:02 +0530

    main page created

---
+++ app/src/main/java/com/jones/busfinder/Bus.kt
@@ -0,0 +1,12 @@
+package com.jones.busfinder
+
+data class Bus (
+    var id: Integer? = null,
+    var departureLoc: String? = null,
+    var departureTime: String? = null,
+    var reachTime: String? = null,
+    var reachLocation: String? = null,
+    var stopList: Map<String,String>? = null,
+    var maxFare: Integer? = null,
+    var isLadies: Boolean? = null,
+        )
